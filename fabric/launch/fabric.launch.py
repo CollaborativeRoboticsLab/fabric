b692b71e2933a135f5523abece36226d289ca2fd
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-03-05 05:53:23 +0000

    added composed launch files and added related documentation

--- fabric/launch/fabric.launch.py
+++ fabric/launch/fabric.launch.py
@@ -1,7 +1,3 @@
-'''
-capabilities2_server launch file
-'''
-
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -9,11 +5,6 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    """Generate launch description for capabilities2 server
-
-    Returns:
-        LaunchDescription: The launch description for capabilities2 executor
-    """
     # load config file
     fabric_config = os.path.join(get_package_share_directory('fabric'), 'config', 'fabric.yaml')
 
