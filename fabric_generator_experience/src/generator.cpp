9414edc7d27c8c867908db82a49ba04e093092e0
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-08-18 12:32:24 +0000

    moved the experience based generation plugin and prompt tools based generation plugins into their own packages

--- fabric_generator_experience/src/generator.cpp
+++
@@ -1,4 +0,0 @@
-#include <pluginlib/class_list_macros.hpp>
-#include <fabric_generator_experience/experience_plugin.hpp>
-
-PLUGINLIB_EXPORT_CLASS(fabric::ExperienceGenerator, fabric::GenerationBase)
--- fabric_generator_prompttools/src/generator.cpp
+++
@@ -1,4 +0,0 @@
-#include <pluginlib/class_list_macros.hpp>
-#include <fabric_generator_prompttools/prompttools_plugin.hpp>
-
-PLUGINLIB_EXPORT_CLASS(fabric::PromptToolsGenerator, fabric::GenerationBase)
